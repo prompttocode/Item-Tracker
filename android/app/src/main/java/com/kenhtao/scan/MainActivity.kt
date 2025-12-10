c607980341769693ddbd113047c1d8bf29afbea8
namdeptrainhatvutru <namvu3121@gmail.com>
2025-12-10 12:31:55 +0700

    ok

--- android/app/src/main/java/com/kenhtao/scan/MainActivity.kt
+++ android/app/src/main/java/com/kenhtao/scan/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.scan
+package com.kenhtao.scan
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/kenhtao/scan/MainApplication.kt
+++ android/app/src/main/java/com/kenhtao/scan/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.scan
+package com.kenhtao.scan
 
 import android.app.Application
 import com.facebook.react.PackageList
